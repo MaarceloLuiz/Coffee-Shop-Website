a200e550ff4b8df014472f18478821b42269ef2c
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-30 21:12:04 +0000

    Making Responsive

--- CA1 - Coffee Shop Website/script.js
+++ CA1 - Coffee Shop Website/script.js
@@ -1,21 +1,22 @@
-﻿let search = document.querySelector('.search-box');
+﻿//search box appearing after clicking on the search icon
+let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
+//navbar appearing after clicling on the menu icon
+let navbar = document.querySelector('.navbar');
 
-//// Add click event to toggle FAQ answer visibility
-//$(document).ready(function () {
-//    $(".faq-question").click(function () {
-//        $(this).next(".faq-answer").toggleClass("show");
-//    });
-//});
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
 
-//$(document).ready(function () {
-//    $(".faq-answer").hide();
-//    $(".faq-question").click(function () {
-//        $(this).next(".faq-answer").slideToggle(500);
-//	});
-//});
+//remove navabar and search box when scrolling
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
 
